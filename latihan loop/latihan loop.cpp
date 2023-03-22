539d4719a6c4dbf206ead9d0191d91be70794d88
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-22 18:33:54 +0700

    membuat prosedur cara mencari Luas lingkaran

--- latihan loop/latihan loop.cpp
+++ latihan loop/latihan loop.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-float Luaspersegipanjang(float p, float l) {
+	float Luaspersegipanjang(float p, float l) {
 	return p * l;
+	
 	float Luassegitiga(float a, float t) {
 		return 0.5 * a * t;
+	}
+
+	float Luaslingkaran(float r) {
+		return 3.14 * r * r;
 	}
 
